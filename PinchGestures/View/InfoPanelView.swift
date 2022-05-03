2ed013800c3a7ecb5e6731fc88a0af41d2308074
Adam Januszewski <Adpjan@att.net>
2022-05-03 16:07:09 -0400

    first commit

---
+++ PinchGestures/View/InfoPanelView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoPanelView.swift
+//  PinchGestures
+//
+//  Created by adam janusewski on 5/3/22.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelView()
+    }
+}
