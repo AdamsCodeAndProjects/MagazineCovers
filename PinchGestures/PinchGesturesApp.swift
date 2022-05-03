684edb0e9121c915326d7b3c3a024d40db6986b2
Adam Januszewski <Adpjan@att.net>
2022-05-03 12:25:58 -0400

    Initial Commit

---
+++ PinchGestures/PinchGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchGesturesApp.swift
+//  PinchGestures
+//
+//  Created by adam janusewski on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
